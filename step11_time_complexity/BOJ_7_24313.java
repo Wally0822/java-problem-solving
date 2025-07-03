30ea6f1e7ce6ea372e2b7f5cd03ab2ea099b3cf9
Wally <1506325470piao@gmail.com>
2025-07-04 03:26:58 +0900

    Update BOJ_7_24313.java

--- step11_time_complexity/BOJ_7_24313.java
+++ step11_time_complexity/BOJ_7_24313.java
@@ -5,11 +5,24 @@ import java.util.Scanner;
 public class BOJ_7_24313 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long n = sc.nextLong();
-        
-        System.out.println(n * n * n + 2 * n * n + 3 * n + 4);
-        System.out.println(3);
-        
+
+        int a1 = sc.nextInt();  // 계수
+        int a0 = sc.nextInt();  // 상수항
+        int c = sc.nextInt();   // 빅오의 상수
+        int n0 = sc.nextInt();  // 시작점
+
+        boolean ok = true;
+
+        for (int n = n0; n <= 100; n++) {  // 문제 조건상 n은 최대 100까지
+            long fn = 1L * a1 * n + a0;
+            long cn = 1L * c * n;
+            if (fn > cn) {
+                ok = false;
+                break;
+            }
+        }
+
+        System.out.println(ok ? 1 : 0);
         sc.close();
     }
 }
